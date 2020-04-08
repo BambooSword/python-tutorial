166b8c1a8837650bae08169a11038dc66c5bea63
BambooSword <herodreamdeng@gmail.com>
2020-04-08 11:00:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle
+
+a = 5
+print(5)
+
+b = 'hello'
+print(b)
+
+credit_card = 3534434343434
+print(credit_card)
+
+qazi_turtle = turtle.Turtle()
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
